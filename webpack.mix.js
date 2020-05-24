cbd3fc2bb2c5ccc899d26a989fdc35c8854d4b44
hoanghuetm <kidinu@hotmail.com>
2020-05-25 00:04:41 +0700

    add js and css file of template

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,33 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js(['resources/js/app.js',
+    'resources/js/active.js',
+    'resources/js/bootstrap.js',
+    'resources/js/bootstrap.min.js',
+    'resources/js/jquery-3.2.1.min.js',
+    'resources/js/modernizr-3.5.0.min.js',
+    'resources/js/plugins.js',
+    'resources/js/popper.min.js',
+    ], 'public/js')
+    .styles([
+        'resources/css/animation.css',
+        'resources/css/bicon.min.css',
+        'resources/css/fakeloader.css',
+        'resources/css/fancybox.css',
+        'resources/css/font-awesome.min.css',
+        'resources/css/fotorama.css',
+        'resources/css/bootstrap.min.css',
+        'resources/css/jquery-ui.min.css',
+        'resources/css/lightbox.css',
+        'resources/css/mainmenu.css',
+        'resources/css/material-design-iconic-font.min.css',
+        'resources/css/nivo-preview-2.css',
+        'resources/css/nivo-slider.css',
+        'resources/css/owl.carousel.min.css',
+        'resources/css/owl.theme.default.min.css',
+        'resources/css/pe-icon-7-stroke.css',
+        'resources/css/simple-line-icons.css',
+        'resources/css/slick.min.css',
+    ], 'public/css/style.css')
     .sass('resources/sass/app.scss', 'public/css');
